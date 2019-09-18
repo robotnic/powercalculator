f77b9f96cc6e14270e51566ef53f020609632da1
bernhard <bernardo.interponti@gmail.com>
2019-09-18 22:23:44 +0200

    nodejs for heroku

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+const compression = require('compression')
+app.use(compression());
+
+
+const PORT = process.env.PORT || 3000
+const baseUrl = 'https://entsoe.herokuapp.com'
+
+
+
+app.all('/api/**', function(req, res) {
+  console.log(req.url);
+  var options = {
+    uri: baseUrl + req.url,
+    method: req.method
+  }
+  console.log(options);
+  request(options, function(err, response, body) {
+    res.send(body);
+  });
+})
+
+
+app.use(express.static('dist/powercalculator'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/powercalculator/index.html');
+});
+
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '!');
+});
